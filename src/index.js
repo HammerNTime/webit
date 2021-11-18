149a80d806cad8afb3496188405f0537fd62c0ce
HammerNTime <hammer.dylan@gmail.com>
2021-11-18 10:30:12 -0800

    minor index cleanup

--- src/index.js
+++ src/index.js
@@ -12,6 +12,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 )
-
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
