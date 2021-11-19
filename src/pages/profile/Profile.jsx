98a4a6f3fb8cc30ccb27d38eeaba1df6c95f83fb
HammerNTime <hammer.dylan@gmail.com>
2021-11-19 13:58:28 -0800

    heroku deploy works

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -28,54 +28,57 @@ function Profile({ currentUser, handleLogout, setCurrentUser }) {
 
 	return (
 		<div className={profileStyles.profile__page}>
-			{user ? 
-			<>
-<div className={profileStyles.profile__left}>
-				<div className={profileStyles.profile__user__info}>
-					<img src={currentUser.avatar} alt="user currentUser.avatar"></img>
-					<h3>{currentUser?.handle}</h3>
+			{user ? (
+				<>
+					<div className={profileStyles.profile__left}>
+						<div className={profileStyles.profile__user__info}>
+							<img src={currentUser.avatar} alt="user currentUser.avatar"></img>
+							<h3>{currentUser?.handle}</h3>
 
-					<h4>Number of templates: {user?.templates.length}</h4>
+							<h4>Number of templates: {user?.templates.length}</h4>
 
-					<button>
-						<Link to="/">Home</Link>
-					</button>
-					<button>
-						<Link to="/">Create a new template</Link>
-					</button>
+							<button>
+								<Link to="/">Home</Link>
+							</button>
+							<button>
+								<Link to="/">Create a new template</Link>
+							</button>
 
-						<button onClick={handleLogout}><Link to="/">Logout</Link></button>
-				</div>
-			</div>
-			<div className={profileStyles.profile__right}>
-				<div className={profileStyles.profile__post__container}>
-					<div className={profileStyles.sub__container}>
-						<div className={profileStyles.profile__posts__header}>
-							<h3>Your Templates</h3>
+							<button onClick={handleLogout}>
+								<Link to="/">Logout</Link>
+							</button>
 						</div>
 					</div>
+					<div className={profileStyles.profile__right}>
+						<div className={profileStyles.profile__post__container}>
+							<div className={profileStyles.sub__container}>
+								<div className={profileStyles.profile__posts__header}>
+									<h3>Your Templates</h3>
+								</div>
+							</div>
 
-					<div className={profileStyles.user__posts}>
-						{templates?.map((template) => (
-							<Template
-								template={template}
-								backgroundColors={template?.backgroundColors}
-								textColors={template?.textColors}
-								title={
-									template?.template_title
-										? template?.template_title
-										: "Special Template"
-								}
-								id={template?._id}
-								userId={currentUser?._id}
-							/>
-						))}
+							<div className={profileStyles.user__posts}>
+								{templates?.map((template) => (
+									<Template
+										template={template}
+										backgroundColors={template?.backgroundColors}
+										textColors={template?.textColors}
+										title={
+											template?.template_title
+												? template?.template_title
+												: "Special Template"
+										}
+										id={template?._id}
+										userId={currentUser?._id}
+									/>
+								))}
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-			</>
-			
-			: "Loading Your Profile" }
+				</>
+			) : (
+				"Loading Your Profile"
+			)}
 		</div>
 	)
 }
