379ec07856482e461510478795758e5d98b00718
HammerNTime <hammer.dylan@gmail.com>
2021-11-20 17:06:22 -0800

    add tag-line

--- src/pages/home/components/hero/Hero.jsx
+++ src/pages/home/components/hero/Hero.jsx
@@ -10,7 +10,8 @@ function Hero() {
             <section className={homeStyles.landing}>
         <div className={homeStyles.text_into}>
             <h2>WEBIT</h2>
-            <h3>A coloring book for developers. Learn to design a site the fun way. Sing up now and join others as you show off your design and coloring skills!</h3>
+            <h4 className={homeStyles.tag_line}>Color-Code Your Site</h4>
+            <h3>A coloring book for developers. Learn to design a site the fun way. Sign up now and join others as you show off your design and coloring skills!</h3>
                     <Link to="/signup">
                         
                     <button> Join Now</button>
